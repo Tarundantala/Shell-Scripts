7e193d4e02ff0a3ee45bc287ef6f59cfc44db3a5
Tarundantala <72135535+Tarundantala@users.noreply.github.com>
2023-03-21 13:06:38 +0530

    Update nodeHealth.sh
    Sample shell script to check the nodehealth

--- nodeHealth.sh
+++ nodeHealth.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
 
+################# 
+#Author: Tarun
+#Date: 20/03/2023
+#
+#This script outputs the node health
+#
+#Version: V1
+#################
+
+set -x # debug mode
+set -e #exits the script when there is an error
+set -o pipefail
+
+#echo "Print the Disk Space"
+df -h
+
+#echo "Print the memory"
+free -g
+
+#echo "Print the CPU"
+nproc
+
+ps-ef | grep "azure" | awk -F " " 'print $2'
